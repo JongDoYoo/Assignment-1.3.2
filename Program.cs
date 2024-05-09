d891bd6486d17c105261dca68b5a6d6868d78dea
JongD <JongD@JONGDO>
2024-05-09 11:41:48 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment103
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 2, 5, 7 };
+            int length = myArray.Length;
+            int temp = 0;
+            
+            for (int i = 0; i < (length / 2); i++)
+            {
+                temp = myArray[i];
+                myArray[i] = myArray[length -1-i];
+                myArray[length-1-i] = temp;
+            }
+            
+            foreach (int i in myArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
+
